880743fd375dcd551d735e936d1446bdc0fc2f7d
Charlie Moog <moogcharlie@gmail.com>
2024-02-02 23:03:31 -0600

    deploy with price changes table

--- Main.hs
+++ Main.hs
@@ -5,16 +5,18 @@
 
 module Main where
 
-import Control.Monad (when)
+import Control.Monad
 import Data.ByteString.Lazy qualified as L
 import Data.FileEmbed (embedStringFile)
 import Data.Maybe
 import Database.SQLite.Simple qualified as SQL
 import GHC.Generics
 import Prices (Item (..), allItems)
+import System.Directory (createDirectory, removeDirectoryRecursive)
 import System.Environment (getArgs)
 import System.IO
-import TraderJoesCom (renderPage)
+import Text.Blaze.Html5 qualified as H
+import TraderJoesCom (renderPage, renderTable)
 
 main :: IO ()
 main = do
@@ -24,18 +26,24 @@ main = do
   case listToMaybe args of
     Just "gen" -> do
       prices <- latestPrices conn
+      changes <- priceChanges conn
       SQL.close conn
-      let html = renderPage $ display <$> prices
-      L.putStr html
+      let html = renderPage [H.h1 "Price Changes", renderTable ["Date Changed", "Item Name" :: String, "Old Price", "New Price"] (displayPriceChange <$> changes), H.h1 "All Items", renderTable ["Item Name" :: String, "Retail Price"] (display <$> prices)]
+      removeDirectoryRecursive "site"
+      createDirectory "site"
+      L.writeFile "site/index.html" html
     Just "fetch" -> do
       hPutStrLn stderr "running"
       scrapeAll conn
       SQL.close conn
       hPutStrLn stderr "done"
-    _ -> putStrLn "run with 'fetch' or 'gen'"
+    _ -> fail "run with 'fetch', 'gen'"
 
 display :: DBItem -> [String]
-display (DBItem {dsku, ditem_title, dretail_price}) = [dsku, ditem_title, dretail_price]
+display (DBItem {ditem_title, dretail_price}) = [ditem_title, dretail_price]
+
+displayPriceChange :: PriceChange -> [String]
+displayPriceChange (PriceChange {pitem_title, pbefore_price, pafter_price, pafter_date}) = [pafter_date, pitem_title, pbefore_price, pafter_price]
 
 data DBItem = DBItem
   { dsku :: String,
@@ -51,6 +59,21 @@ instance SQL.ToRow DBItem
 
 latestPrices :: SQL.Connection -> IO [DBItem]
 latestPrices conn = SQL.query conn $(embedStringFile "./latest-prices.sql") ()
+
+data PriceChange = PriceChange
+  { psku :: String,
+    pitem_title :: String,
+    pbefore_price :: String,
+    pafter_price :: String,
+    pbefore_date :: String,
+    pafter_date :: String
+  }
+  deriving (Generic, Show)
+
+instance SQL.FromRow PriceChange
+
+priceChanges :: SQL.Connection -> IO [PriceChange]
+priceChanges c = SQL.query c $(embedStringFile "./leads.sql") ()
 
 scrapeAll :: SQL.Connection -> IO ()
 scrapeAll conn = do
--- TraderJoesCom.hs
+++ TraderJoesCom.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -Wall #-}
 
-module TraderJoesCom (renderPage) where
+module TraderJoesCom (renderPage, renderTable) where
 
 import Data.ByteString.Lazy (ByteString)
 import Data.FileEmbed (embedStringFile)
@@ -10,27 +10,23 @@ import Text.Blaze.Html.Renderer.Utf8 (renderHtml)
 import Text.Blaze.Html5 as H
 import Text.Blaze.Html5.Attributes qualified as A
 
-renderPage :: [[String]] -> ByteString
-renderPage rows = renderHtml $ html $ do
+renderPage :: ToMarkup a => a -> ByteString
+renderPage page = renderHtml $ html $ do
   H.head $ do
     H.meta ! A.charset "UTF-8"
     H.title "Trader Joe's Prices"
   body $ do
     style $(embedStringFile "./style.css")
-    h1 "Latest Prices"
-    renderTable rows
+    toMarkup page
 
-renderTable :: [[String]] -> H.Html
-renderTable rows = html $ do
+renderTable :: ToMarkup a => [a] -> [[a]] -> H.Html
+renderTable headers rows = html $ do
   H.table ! A.class_ "table table-striped table-gray" $ do
-    H.thead $ H.tr $ do
-      H.th "Stock ID"
-      H.th "Item Name"
-      H.th "Retail Price"
+    H.thead $ H.tr $ toMarkup $ H.th <$> (toMarkup <$> headers)
     H.tbody $ toMarkup $ toMarkup . renderRow <$> rows
 
-renderRow :: [String] -> H.Html
+renderRow :: ToMarkup a => [a] -> H.Html
 renderRow row = H.tr $ toHtml $ renderCell <$> row
 
-renderCell :: String -> Html
+renderCell :: ToMarkup a => a -> Html
 renderCell = H.td . H.toMarkup
