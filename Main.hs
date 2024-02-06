2b9ec8f356ad6f89c541e6f48da2d01a6e972d86
Charlie Moog <moogcharlie@gmail.com>
2024-02-06 17:49:06 -0600

    link to traderjoes.com product page

--- Main.hs
+++ Main.hs
@@ -6,6 +6,7 @@
 module Main where
 
 import Control.Monad
+import Data.ByteString.Lazy (ByteString)
 import Data.ByteString.Lazy qualified as L
 import Data.FileEmbed (embedStringFile)
 import Data.Maybe
@@ -15,8 +16,9 @@ import Prices (Item (..), allItems)
 import System.Directory (createDirectory, removeDirectoryRecursive)
 import System.Environment (getArgs)
 import System.IO
+import Text.Blaze.Html.Renderer.Utf8 (renderHtml)
 import Text.Blaze.Html5 qualified as H
-import TraderJoesCom (renderPage, renderTable)
+import Text.Blaze.Html5.Attributes qualified as A
 
 main :: IO ()
 main = do
@@ -28,13 +30,15 @@ main = do
       prices <- latestPrices conn
       changes <- priceChanges conn
       SQL.close conn
-      let html =
-            renderPage
-              [ H.h1 "Price Changes",
-                renderTable ["Date Changed", "Item Name" :: String, "Old Price", "New Price"] (displayPriceChange <$> changes),
-                H.h1 "All Items",
-                renderTable ["Item Name" :: String, "Retail Price"] (displayDBItem <$> prices)
-              ]
+      let html = renderPage $ do
+            H.h1 "Price Changes"
+            H.table H.! A.class_ "table table-striped table-gray" $ do
+              H.thead . H.tr . H.toMarkup $ H.th <$> (H.toMarkup <$> ["Date Changed" :: String, "Item Name", "Old Price", "New Price"])
+              H.toMarkup $ displayPriceChange <$> changes
+            H.h1 "All Items"
+            H.table H.! A.class_ "table table-striped table-gray" $ do
+              H.thead . H.tr . H.toMarkup $ H.th <$> (H.toMarkup <$> ["Item Name" :: String, "Retail Price"])
+              H.toMarkup $ displayDBItem <$> prices
       removeDirectoryRecursive "site"
       createDirectory "site"
       L.writeFile "site/index.html" html
@@ -45,11 +49,32 @@ main = do
       hPutStrLn stderr "done"
     _ -> fail "run with 'fetch', 'gen'"
 
-displayDBItem :: DBItem -> [String]
-displayDBItem (DBItem {ditem_title, dretail_price}) = [ditem_title, dretail_price]
+renderPage :: (H.ToMarkup a) => a -> ByteString
+renderPage page = renderHtml $ H.html $ do
+  H.head $ do
+    H.meta H.! A.charset "UTF-8"
+    H.meta H.! A.name "viewport" H.! A.content "width=device-width, initial-scale=1.0"
+    H.title "Trader Joe's Prices"
+  H.body $ do
+    H.style $(embedStringFile "./style.css")
+    H.toMarkup page
 
-displayPriceChange :: PriceChange -> [String]
-displayPriceChange (PriceChange {pitem_title, pbefore_price, pafter_price, pafter_date}) = [pafter_date, pitem_title, pbefore_price, pafter_price]
+displayDBItem :: DBItem -> H.Html
+displayDBItem (DBItem {ditem_title, dretail_price, dsku}) = H.html $ do
+  H.tr $ do
+    H.td $ H.a H.! A.href (productUrl dsku) H.! A.target "_blank" $ H.toHtml ditem_title
+    H.td $ H.toHtml dretail_price
+
+displayPriceChange :: PriceChange -> H.Html
+displayPriceChange (PriceChange {pitem_title, pbefore_price, pafter_price, pafter_date, psku}) = H.html $ do
+  H.tr $ do
+    H.td $ H.toHtml pafter_date
+    H.td $ H.a H.! A.href (productUrl psku) H.! A.target "_blank" $ H.toHtml pitem_title
+    H.td $ H.toHtml pbefore_price
+    H.td $ H.toHtml pafter_price
+
+productUrl :: String -> H.AttributeValue
+productUrl sku = H.toValue $ "https://traderjoes.com/home/products/pdp/" ++ sku
 
 data DBItem = DBItem
   { dsku :: String,
